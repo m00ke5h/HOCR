ce1b13c3d4a01df2ce0718d3dadb438786395563
m-ar-sh-h <pkdash1947.er@gmail.com>
2022-03-09 12:49:21 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/converted')
+def converted():
+    return render_template('converted.html')
